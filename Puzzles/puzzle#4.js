ddd464af14daab83130b5736acc539380fb0b11f
Israel <israel.klein@jjay.cuny.edu>
2022-01-14 15:19:18 -0500

    added identity operator

--- Puzzles/puzzle#4.js
+++ Puzzles/puzzle#4.js
@@ -1,17 +1,17 @@
 function number(n){
 // function to process the number
-    if ((n % 3 == 0) && (n % 5 == 0)){
+    if ((n % 3 === 0) && (n % 5 === 0)){
         //checks if the number is divisible by 5 and 3
         console.log('Fizz Buzz');
         // prints out Fizz Buzz if the number is divisible by 5 and 3
     }
-    else if(n % 3 == 0){
+    else if(n % 3 === 0){
         // checks if the the number is divisible by 3
         console.log('Fizz');
         // prints out Fizz if the number is divisible by 3
     }
 
-    while(n != 0){
+    while(n > 0){
         // the loop iterates to print out the number n and every value below it up until 0
         console.log(n);
         // prints out the current value of n
