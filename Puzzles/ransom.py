48bb73b3a83fa7ad6ef84296238217ea12077b52
Israel <israel.klein@jjay.cuny.edu>
2022-01-17 22:03:51 -0500

    .

---
+++ Puzzles/ransom.py
@@ -0,0 +1,11 @@
+"""create a string ransomNote
+create a second string magazine
+create empty string
+
+run a loop to iterate each string
+    check if ransomNote string is the same same length or shorter than magazine string
+    
+        iterate through each letter in magazine and see if it matches any letter
+        in the ransomeNote if it does then store in empty string
+        
+    otherwise ouput that it's false"""
---
+++ React_projects/the_venue/src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import './resources.style.css';
+import {Element} from 'react-scroll';
+
+
+import Header from './components/header_footer/Header';
+import Featured from '/components/featured';
+import VenueInfo from './components/venueInfo';
+import Highlight from './components/Highlights';
+import Pricing from './components/pricing';
+import Location from './components/location';
+import Footer from './components/header_footer/Footer';
+
+class App extends Component {
+  render(){
+    retun (
+      <div className='App' style={{height:'1500px', background: 'cornflowerblue'}}>
+        <Header/>
+
+        <Element name='featured'>
+          <Featured/>
+        </Element>
+
+        <Element name='venuenfo'>
+          <VenueInfo/>
+        </Element>
+
+        <Element name='highlight'>
+          <Highlight/>
+        </Element>
+
+        <Element name='pricing'>
+          <Pricing/>
+        </Element>
+        
+        <Element name='location'>
+          <Location/>
+        </Element>
+
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+  export default App;
