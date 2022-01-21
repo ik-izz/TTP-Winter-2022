6e5737c22dd8e032518e250626018e9c0268ff94
Israel <israel.klein@jjay.cuny.edu>
2022-01-20 20:40:15 -0500

    .

--- Projects/React_projects/city_tours/src/tourData.js
+++ Projects/React_projects/city_tours/src/tourData.js
@@ -1,5 +1,44 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+export const tourData = [
+    {
+    id: 1,
+    city: 'new york',
+    img: './img/ahmad_bin_ali.jpg',
+    name: 'statue of Liberty tour',
+    info: "lorem"
+    },
+    {
+    id: 2,
+    city: 'Paris',
+    img: './img/al_bayt.jpg',
+    name: 'paris lights tour',
+    info: "lorem"
+    },
+    {
+    id: 3,
+    city: 'Londo',
+    img: './img/al_thumama4.jpg',
+    name: 'london royal palace tour',
+    info: "lorem"
+    },
+    {
+    id: 4,
+    city: 'Tokyo',
+    img: './img/alwakrah.jpg',
+    name: 'tokyo sushi tour',
+    info: "lorem"
+    },
+    {
+    id: 5,
+    city: 'Brooklyn Bridge',
+    img: './img/khalifa_stadium10.jpg',
+    name: 'Brooklyn Bridge Tour',
+    info: "lorem"
+    },
+    {
+    id: 6,
+    city: 'Yankee Stadium',
+    img: './img/ras_abu.jpg',
+    name: 'Yankee Stadium tour',
+    info: "lorem"
+    },
+];
