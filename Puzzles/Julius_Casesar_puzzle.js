9a54b0b10a32fa051cd9ce57f1bd00cd1243254c
Israel <israel.klein@jjay.cuny.edu>
2022-01-21 16:31:36 -0500

    .

---
+++ Puzzles/Julius_Casesar_puzzle.js
@@ -0,0 +1,19 @@
+function ceaserCipher(str, num) {
+    num = num % 26;
+    var lowerCaseStr = str.toLowerCase();
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var newStr = '';
+    for(var i=0; i<lowerCaseStr.length; i++){
+        var currentLetter = lowerCaseStr[i];
+        if(currentLetter === ' '){
+            newStr += currentLetter;
+            continue;        }
+        var currentIndex = alphabet.indexOf(currentLetter);
+        var newIndex = currentIndex + num;
+        if(newIndex>25) newIndex= newIndex-26;
+        if(newIndex<0) newIndex= newIndex +26;
+        if(str[i] === str[i].toUpperCase()){
+            newStr += alphabet[newIndex].toUpperCase();}
+        else newStr += alphabet[newIndex];
+    } return newStr
+} console.log(ceaserCipher('bye', 3))
