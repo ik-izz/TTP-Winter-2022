e05ee9f06f3518e6eba42f7f97b6ec64d525e732
ik-izz <94077710+ik-izz@users.noreply.github.com>
2022-01-24 16:04:39 -0500

    .

---
+++ Puzzles/puzzle#6.js
@@ -0,0 +1,46 @@
+// reverses entire string
+// function reverseString(str) {
+//     var newString = "";
+//     for (var i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//     }
+//     return newString;
+// }
+// console.log(reverseString('hello there'));
+
+
+// reverses specified words
+// function reverseString(str){
+//     var newString = '';
+//     newString = str.split(' ')
+//     secondStr = [newString[1], newString[0]]
+//     console.log(secondStr.join())
+// }
+
+// reverseString('hello there')
+
+
+// function reverseString(str){
+//     for(var i = str.length - 1; i >= 0; i-- ){
+//     var newString = '';
+//     newString = str.split(' ')
+//     secondStr = []
+//     secondStr = newString[i]
+//     }
+//     console.log(secondStr)
+// }
+
+// reverseString('hello there')
+
+// reverses specified words
+function reverseString(str){
+    var newString = '';
+    newString = str.split(' ')
+    for (var i = newString.length; i >= 0; i--){
+        var results = 
+    }
+    console.log(secondStr.join())
+
+}
+
+reverseString('hello there')
